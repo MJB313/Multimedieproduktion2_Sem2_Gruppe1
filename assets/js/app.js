95841af321bcc0e3b64736f20de63b0b5f866e36
KristinaHejlesen <kkhejlesen@gmail.com>
2025-06-04 17:26:30 +0200

    Tilføjet Accordion

---
+++ assets/js/app.js
@@ -0,0 +1,22 @@
+
+
+
+//JS Accordion
+// rammer alle med querySelectorAll med class accordion .forEach loop kører for hver knap/button tilføjes en EventListener der lytter efter klik på button.//
+document.querySelectorAll(".accordion").forEach(button => {
+    button.addEventListener("click", ()=>{
+        //erklære at accodionContent er det næste element ved siden af, her vores indhold under knappen.//
+        const accordionContent = button.nextElementSibling;
+        // hvis indholdet har class open, så skal den fjernes, da CSS har display: none. vises indholdet ikke
+        if(accordionContent.classList.contains("open")){
+            accordionContent.classList.remove("open");
+            /*fjerner class til knappen også så de runde kanter kommer på igen hvis den er åben */
+            button.classList.remove("open");
+        //ellers tilføj class open. Dette åbner vores indhold, da class i vores CSS har display: block
+        } else {
+            accordionContent.classList.add("open");
+            /*tilføjer class til knappen også så de runde kanter fjernes når accordion er åben */
+            button.classList.add("open");
+        }
+    });
+    });
